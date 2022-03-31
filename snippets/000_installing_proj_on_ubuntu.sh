386c3d18fdbe7628f553d879168ac45be9d6cc05
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-03-31 14:43:23 +0200

    Add code snippets
    [no ci]

---
+++ snippets/000_installing_proj_on_ubuntu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script contains the command required to install bedtools-ng on an ubuntu machine
+# Commands should be run from the repository's root
+
+# Update repository metadata
+sudo apt-get update
+
+# Install dependencies
+sudo apt-get install -y git                \
+                        python3            \
+                        python3-pip        \
+                        python3-setuptools
+
+# Install bedtools-ng
+pip install '.[all]'
+
+# Run unit tests
+pytest .
+
+# Run integration tests
+scripts/integration_test.sh
+
+# Check bedtools-ng was correctly installed
+
+bedtools-ng --help
+bedtools-ng --version
+
