b00ed1fe58b35cf76500ffd0bf7e636343546ed7
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-03-31 13:16:15 +0200

    Add toy Python application

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
